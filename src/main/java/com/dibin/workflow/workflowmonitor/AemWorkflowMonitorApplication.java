f7bf2a471530f857d086e08d1288e773ea0dfa62
Dibin <dibs.vr@gmail.com>
2022-12-11 01:09:20 +0530

    Initial commit

---
+++ src/main/java/com/dibin/workflow/workflowmonitor/AemWorkflowMonitorApplication.java
@@ -0,0 +1,13 @@
+package com.dibin.workflow.workflowmonitor;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AemWorkflowMonitorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AemWorkflowMonitorApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/dibin/workflow/workflowmonitor/AemWorkflowMonitorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dibin.workflow.workflowmonitor;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AemWorkflowMonitorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
